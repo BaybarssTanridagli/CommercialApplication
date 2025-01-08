3eca1d5ef0ca24787e208ea3134d4925d35ca7a2
Baybars <baybarsstanridagli@gmail.com>
2025-01-08 13:52:24 +0300

    CommercialApplication

---
+++ CommercialApplication/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommercialApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double purchase, gainRate, sell;
+            Console.WriteLine("Please Enter The Purchase Rate: ");
+            purchase=Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please Enter The Gain Rate: ");
+            gainRate=Convert.ToDouble(Console.ReadLine());
+            sell = purchase + (purchase * gainRate / 100);
+            Console.WriteLine("Price : {0}", sell);
+            Console.ReadKey();
+        }
+    }
+}
